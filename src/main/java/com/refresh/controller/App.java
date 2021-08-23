6ac8e5f383b34cc79a452bd251e6b7503103a213
sdolgov <Snd251084>
2021-08-23 11:15:13 +0300

    1

--- src/main/java/com/refresh/controller/App.java
+++ src/main/java/com/refresh/controller/App.java
@@ -2,10 +2,9 @@ package com.refresh.controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-@EnableConfigServer
+//@EnableConfigServer
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
